c6691f6edfc56002f7c5806971701cb50a6acffb
Egor Postnikov <ea-post134@yandex.ru>
2024-04-16 03:16:59 +0100

    Fix dates

--- client/src/types/FileObject.ts
+++ client/src/types/FileObject.ts
@@ -1,3 +1,6 @@
+
+export type SelectDates = { fromDate: string, toDate: string };
+
 export type FileObject = {
     id: string
     name: string
@@ -6,6 +9,6 @@ export type FileObject = {
     type: string
     num_rows: number
     selectColumn: string
-    selectDates: {fromDate: string, toDate: string}
+    selectDates?: SelectDates
     file_id: string
 }
